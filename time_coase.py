581d01ee6786e9559cc2cb28301a270c736020f3
cydia <admin@mail.com>
2020-10-12 14:33:24 +0800

    函数计时

--- time_coase.py
+++ time_coase.py
@@ -19,26 +19,13 @@
                             女神保佑         永无BUG                            佛祖保佑         永无BUG
                                                                                                      
 '''
-from time import sleep
-from typing import List
+import time
 
-from time_coase import coast
-
-
-class Solution:
-    # 暴力算法时间复杂度O（n²），空间复杂度O（1）
-    @coast
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-
-s = Solution()
-print(s.twoSum([x for x in range(500,10000)], 1001))
-# s.twoSum([3,3], 6)
+def coast(fun):
+    def wapper(*args, **kwargs):
+        start = time.time()
+        ret = fun(*args, **kwargs)
+        end = time.time()
+        print("Total time: {:.5}".format(end - start))
+        return ret
+    return wapper
