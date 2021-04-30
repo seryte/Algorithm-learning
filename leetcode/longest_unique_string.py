732b67e5b5a10b0fc7b297550c96347d0ca7b243
seryte <382643558@qq.com>
2021-04-30 11:59:46 +0800

    更新

---
+++ leetcode/longest_unique_string.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @File    : longest_unique_string.py
+# @Date    : 2021-04-22 11:50:52
+# @Author  : seryte
+# @Link    : https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
+# @Project : Algorithm-learning
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        pass
+
+
+a = [1, 2]
+b = ['a', 'b', 'c', 'f']
+
+
+def c():
+    pass
+
+
+r = [[str(x) + y] for x in a for y in b]
+
+print(r)
